a7f9d5d44dde9f7164414048d3b54ef69d380fd1
root <2907990037@qq.com>
2022-02-13 16:44:18 +0800

    # 添加类目interface及注释

--- qerror/error.go
+++ qerror/error.go
@@ -1,21 +1,26 @@
 package qerror
 
+//Error qlite system error type
 type Error []byte
 
+//Copy an new error
 func (e Error) Copy() Error {
 	buf := make(Error, len(e))
 	copy(buf, e)
 	return buf
 }
 
+//New error
 func New(buf []byte) Error {
 	return Error(buf)
 }
 
+//NewString new error to string
 func NewString(err string) Error {
 	return Error(err)
 }
 
+//Error to error interface
 func (e Error) Error() string {
 	return string(e)
 }
--- template/class.go
+++ template/class.go
@@ -1,5 +1,29 @@
 package template
 
-type Class interface {
+import "github.com/culionbear/qtool/qerror"
 
+const (
+	Number = iota
+	String
+	List
+	Bool
+)
+
+type VarType int
+
+type Api struct {
+	VarList	[]VarType
+	ReturnVar VarType
+	ReadOnly bool
+}
+
+type Result struct {
+	Err qerror.Error
+	Msg []byte
+	Persistence bool
+}
+
+type Class interface {
+	Do([]byte) Result
+	ApiList() []Api
 }
