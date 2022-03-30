290bf09f1fd2580209dc2852bc19b5de0fbcb715
root <2907990037@qq.com>
2022-03-31 03:25:31 +0800

    # 设置default封包情况

--- protocol/pack.go
+++ protocol/pack.go
@@ -13,6 +13,9 @@ func (m *Manager) Pack(module any) []byte {
 }
 
 func (m *Manager) pack(module any) []byte {
+	if module == nil {
+		return m.fromBool(true)
+	}
 	switch msg := module.(type) {
 	case byte:
 		return m.fromByte(msg)
